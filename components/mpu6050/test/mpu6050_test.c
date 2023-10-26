3b1a8115b5b1d392428212eda48690d88c09d2d8
Max <max_rickers@hotmail.de>
2023-10-26 14:59:12 +0200

    Moved mpu6050 driver to components

--- components/mpu6050/test/mpu6050_test.c
+++ components/mpu6050/test/mpu6050_test.c
@@ -11,8 +11,9 @@
 #include "esp_system.h"
 #include "esp_log.h"
 
-#define I2C_MASTER_SCL_IO 26      /*!< gpio number for I2C master clock */
-#define I2C_MASTER_SDA_IO 25      /*!< gpio number for I2C master data  */
+// for esp32 almost any gpio can be used for i2c
+#define I2C_MASTER_SCL_IO 22      /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO 21      /*!< gpio number for I2C master data  */
 #define I2C_MASTER_NUM I2C_NUM_0  /*!< I2C port number for master dev */
 #define I2C_MASTER_FREQ_HZ 100000 /*!< I2C master clock frequency */
 
